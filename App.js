6c3e601a383d6138b7ad6b02130a0f38ce51db7a
Sirius Hou <118148925+Sirius-Hou@users.noreply.github.com>
2023-02-19 19:42:55 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
 import React, { useState } from 'react';
 import './styles.css';
 
